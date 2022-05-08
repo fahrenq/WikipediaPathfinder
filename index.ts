4209e6dcaff7c6dc14dcc406efa5e7a704a815c8
Ivan Kuznetsov <ivankuznetsov316@gmail.com>
2022-05-08 20:02:37 +0000

    Initial commit

---
+++ index.ts
@@ -0,0 +1,17 @@
+const JSDOM = 
+
+const START = 'https://en.wikipedia.org/wiki/Simona_Dimitrova';
+const FINISH = 'https://en.wikipedia.org/wiki/Temple_of_Apshai_Trilogy';
+
+
+const lookupForUrl = (start: string, finish: string) => {
+  const dom = new JSDOM({url: start})
+  
+}
+
+(async () => {
+  
+  
+  
+  
+})
